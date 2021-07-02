f956caa32a83dd6b8e9742629ba35f1c79438314
JBG0000 <practice9090@naver.com>
2021-07-03 01:05:37 +0900

    django course 16 coummit

--- accountap/views.py
+++ accountap/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountap/hello_world.html')
+
+    if request.method == "POST":    #post : 객체를 생성할때 주로 사용
+        return render(request, 'accountap/hello_world.html', context={'text': 'POST METHOD!!!'})
+    else:
+        return render(request, 'accountap/hello_world.html', context={'text': 'GET METHOD!!!'})
