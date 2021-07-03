cc9eb0e8410390d210092336e8c63c2aaf3be94e
JBG0000 <practice9090@naver.com>
2021-07-03 21:00:41 +0900

    django course 28 commit

--- accountap/views.py
+++ accountap/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,31 +6,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy  # accountap 내부의 hello_world로 재접속하라
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountap.decorators import account_ownership_required
 from accountap.forms import AccountUpdateForm
 from accountap.models import HelloWorld  # models에 있던 DB저장 유형틀 쓸거다!
 
 
+has_ownership = [account_ownership_required, login_required]    #데코레이터 요소 이 배열 하나만 스면 배열내부 모두 적용
+
+@login_required #decorators 로그인 작동
 def hello_world(request):
-    if request.user.is_authenticated:  # 유저 접속 확인
+    if request.method == "POST":  # post : 객체를 생성할때 주로 사용
+        temp = request.POST.get('hello_world_input')  # 작성한걸 읽는다!
 
-        if request.method == "POST":  # post : 객체를 생성할때 주로 사용
-            temp = request.POST.get('hello_world_input')  # 작성한걸 읽는다!
+        new_hello_world = HelloWorld()  # models에서 정의한 틀 쓰겠다! 그 변수다!
+        new_hello_world.text = temp  # text 읽어온거 저장
+        new_hello_world.save()  # DB 저장
 
-            new_hello_world = HelloWorld()  # models에서 정의한 틀 쓰겠다! 그 변수다!
-            new_hello_world.text = temp  # text 읽어온거 저장
-            new_hello_world.save()  # DB 저장
+        # hello_world_list = HelloWorld.objects.all() #헬로우 월드의 모든 저장내용을 리스트에 저장
 
-            # hello_world_list = HelloWorld.objects.all() #헬로우 월드의 모든 저장내용을 리스트에 저장
-
-            return HttpResponseRedirect(
-                reverse('accountap:hello_world'))  # temp에 저장된 써진 내용, 읽어온 걸 반환하는 함수(hello_world.html로 가서 출력된다) : ㄲ
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountap/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(
+            reverse('accountap:hello_world'))  # temp에 저장된 써진 내용, 읽어온 걸 반환하는 함수(hello_world.html로 가서 출력된다) : ㄲ
     else:
-        return HttpResponseRedirect(reverse('accountap:login'))  # 로그인 안되면 로그인 창으로 돌려보냄
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountap/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):  # 회원가입 창 : 파라미터 넘겨주기~
@@ -45,6 +47,8 @@ class AccountDetailView(DetailView):  # 마이페이지
     template_name = 'accountap/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):  # 업데이트 창
     model = User
     context_object_name = 'target_user'
@@ -52,33 +56,12 @@ class AccountUpdateView(UpdateView):  # 업데이트 창
     success_url = reverse_lazy('accountap:hello_world')  # 성공시 연결 url
     template_name = 'accountap/update.html'
 
-    def get(self, *args, **kwargs):  # 망치는 과정 : 어카운트 로그인없이 페이지 수정 방지
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:   #로그인한 사람이 정보를 바꾸려는 유저랑 같은지 확인
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):  # post도
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):  # 회원탈퇴
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountap:login')
     template_name = 'accountap/delete.html'
 
-    def get(self, *args, **kwargs):  # 망치는 과정 : 어카운트 로그인없이 페이지 수정 방지
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()  #금지된 곳 접근
-
-    def post(self, request, *args, **kwargs):  # post도
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
