a0a8e33bc9ad549264fc2283cd06b48f09c780ae
JBG0000 <practice9090@naver.com>
2021-07-04 06:33:32 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.decorators import profile_ownership_required
@@ -15,7 +15,6 @@ class ProfileCreateView(CreateView):    #프로필 생성
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountap:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -23,6 +22,9 @@ class ProfileCreateView(CreateView):    #프로필 생성
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form) #다시 자신이 들어가는
+
+    def get_success_url(self):
+        return reverse('accountap:detail', kwargs={'pk': self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -32,3 +34,6 @@ class ProfileUpdateView(UpdateView):    #프로필 업데이트
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountap:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountap:detail', kwargs={'pk': self.object.user.pk})
