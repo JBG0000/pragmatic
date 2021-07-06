ad0fd7bdc3dc1e32480e03d0973426b681be34e8
JBG0000 <practice9090@naver.com>
2021-07-06 22:06:01 +0900

    django course 38 commit

--- commentapp/models.py
+++ commentapp/models.py
@@ -10,5 +10,5 @@ class Comment(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comment')
 
     content = models.TextField(null=False)
-    
+
     create_at = models.DateTimeField(auto_now=True)
